ee92f3a72cc34948e433a074d9b0c9243951d7fe
Andre <andreluiz.miranda@outlook.com>
2026-03-25 12:36:52 -0300

    add disable button register by route

--- src/app/shared/components/global/top-menu/top-menu.ts
+++ src/app/shared/components/global/top-menu/top-menu.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import { RouterLink } from "@angular/router";
+import { NavigationEnd, Router, RouterLink } from "@angular/router";
+import { filter, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-top-menu',
@@ -11,6 +12,30 @@ import { RouterLink } from "@angular/router";
   templateUrl: './top-menu.html',
   styleUrl: './top-menu.scss',
 })
-export class TopMenu {
+export class TopMenu implements OnInit, OnDestroy {
+  
+  
   logoApp = 'assets/Logo-Mny.png';
+
+  rotaAtual: string = '';
+  inscricaoAtual!: Subscription;
+
+  constructor(private router: Router) {} // Injeção de dependência para acessar as rotas
+  
+  ngOnInit(): void {
+    this.rotaAtual = this.router.url 
+    this.inscricaoAtual = this.router.events
+    .pipe(filter(event => event instanceof NavigationEnd))
+    .subscribe((evento: NavigationEnd) => {
+      this.rotaAtual = evento.url
+      console.log('Rota atual2: ', this.rotaAtual)})
+  }
+
+  ngOnDestroy(): void {
+    this.inscricaoAtual.unsubscribe(); //Memory Leak
+  }
+
+  isOnRegister(): boolean {
+    return this.rotaAtual ==='/register'
+  }
 }

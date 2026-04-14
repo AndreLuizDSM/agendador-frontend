94443a90e345c8e7ec248206624fced7b21b0679
Andre <andreluiz.miranda@outlook.com>
2026-04-14 14:44:02 -0300

    add tasks modal, add mat-error in modal-dialog

--- src/app/pages/tasks/tasks.ts
+++ src/app/pages/tasks/tasks.ts
@@ -1,6 +1,11 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, inject, ViewEncapsulation } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { User } from '../../services/user';
+import { Auth } from '../../services/auth';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogFields, ModalDialog } from '../../shared/components/modal-dialog/modal-dialog';
 
 @Component({
   selector: 'app-tasks',
@@ -9,6 +14,56 @@ import { MatCardModule } from '@angular/material/card';
   styleUrl: './tasks.scss',
   encapsulation: ViewEncapsulation.None
 })
-export class Tasks { 
-  
+export class Tasks {
+
+  private formBuilder = inject(FormBuilder);
+  private authService = inject(Auth);
+  readonly dialog = inject(MatDialog);
+
+  //TODO taskService para salvar e editar tasks
+
+  adicionarTarefa() {
+    const token = this.authService.getToken();
+    if (!token) return;
+
+    const formConfig: DialogFields[] = [
+      { name: 'nomeTarefa', label: 'Nome da tarefa', validators: [Validators.required] },
+      { name: 'data', label: 'Dia', type: 'date', validators: [Validators.required] },
+      { name: 'tempo', label: 'Hora da tarefa', type: 'time', validators: [Validators.required] },
+      { name: 'descricao', label: 'Descrição', type: 'textarea', validators: [Validators.required] },
+
+    ]
+
+    const dialogRef = this.dialog.open(ModalDialog, {
+      data: { title: 'Adicionar tarefa', formConfig: formConfig },
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        const { data, tempo, ...resto } = result;
+
+        const ano = data.getFullYear();
+        const mes = data.getMonth();
+        const dia = data.getDate();
+
+        const hora = tempo.getHours();
+        const minuto = tempo.getMinutes();
+        const segundo = tempo.getSeconds();
+
+        const dataEvento = new Date(ano, mes, dia, hora, minuto, segundo).toISOString();
+        const payload = {
+          ...resto,
+          dataEvento
+        }
+
+        
+        console.log('Tarefa cadastrada ', payload)
+        // this.userService.updateEndereco(endereco.id, result, token).subscribe({
+        //   next: () => console.log('Endereco editado', result),
+        //   error: () => console.log('Erro', result)
+        // });
+      };
+    });
+    //Fazer o afterClosed para adicionar no taskService.
+  }
 }
--- src/app/shared/components/modal-dialog/modal-dialog.ts
+++ src/app/shared/components/modal-dialog/modal-dialog.ts
@@ -1,5 +1,5 @@
 import { Component, inject, ViewEncapsulation } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import {
   MAT_DIALOG_DATA,
@@ -13,6 +13,10 @@ import {
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { ChangeDetectionStrategy } from '@angular/core';
+import { MatTimepickerModule } from '@angular/material/timepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 export interface DialogFields {
   name: string,
@@ -30,10 +34,22 @@ interface dialogData {
 
 @Component({
   selector: 'app-modal-dialog',
-  imports: [MatButtonModule, FormsModule, MatInputModule, MatFormFieldModule, MatDialogContent, MatDialogActions, ReactiveFormsModule, MatIconModule],
+  imports: [MatButtonModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDialogContent,
+    MatDialogActions,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatTimepickerModule
+  ],
   templateUrl: './modal-dialog.html',
   styleUrl: './modal-dialog.scss',
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [provideNativeDateAdapter()]
 })
 export class ModalDialog {
 
@@ -61,5 +77,19 @@ export class ModalDialog {
 
   onCancel(): void {
     this.dialogRef.close();
+  }
+
+  checkValidators(): boolean {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched
+      return true
+    }
+    return false
+  }
+
+  get fieldError(): string | null {
+    const fieldControl = this.form.get('') //TODO Achar um parâmetro do modal para verificar se tem error e colocar o matError
+    if (fieldControl?.hasError('required')) return 'Campo obrigatório*';
+    return null
   }
 }
